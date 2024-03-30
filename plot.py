a23ab57f66974ec959121cf8309d43ead53fada8
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-31 01:00:44 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -5,20 +5,23 @@ n2 = [] # time (S)
 nlgn = [] # time (S)
 
 f = open('result.txt', 'r')
-i = 0
-for line in f:
-  if i%3 == 0:
-    input_size.append(line)
-  elif i%3 == 1:
-    nlgn.append(line)
-  else:
-    n2.append(line)
-  ++i
+all_data = f.read()
+data = all_data.split()
+# i = 0
+# for line in f:
+#   if i%3 == 0:
+#     input_size.append(line)
+#   elif i%3 == 1:
+#     nlgn.append(line)
+#   else:
+#     n2.append(line)
+#   ++i
 f.close()
 
-print(input_size)
-print(n2)
-print(nlgn)
+print(data)
+# print(input_size)
+# print(n2)
+# print(nlgn)
 # # plot(x, y)
 # plt.plot(input_size, n2, label='n^2')
 # plt.plot(input_size, nlgn, label='nlgn')
