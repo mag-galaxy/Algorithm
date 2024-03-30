d9c06bb666e73796154035d62ccb72cfa56a140b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-03-31 00:45:16 +0800

    Update plot.py

--- plot.py
+++ plot.py
@@ -1,1 +1,26 @@
-import matlibplot
+import matplotlib.pyplot as plt
+
+input_size = [] # n
+n2 = [] # time (S)
+nlgn = [] # time (S)
+
+f = open('result.txt', 'r')
+i = 0
+for line in f:
+  if i%3 == 0:
+    input_size.append(line)
+  elif i%3 == 1:
+    nlgn.append(line)
+  else:
+    n2.append(line)
+  ++i
+f.close()
+
+# plot(x, y)
+plt.plot(input_size, n2, label='n^2')
+plt.plot(input_size, nlgn, label='nlgn')
+plt.xlabel('input size (n)')
+plt.ylabel('time (s)')
+plt.legend()
+plt.grid(True)
+plt.show()
