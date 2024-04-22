80fc06883d37f1e71805d0f32ce8e5958ced5c1b
mag-galaxy <135800722+mag-galaxy@users.noreply.github.com>
2024-04-22 22:41:23 +0800

    Create plot2.py

---
+++ plot2.py
@@ -0,0 +1,45 @@
+import matplotlib.backends.backend_agg
+import matplotlib.figure
+import numpy
+import pathlib
+
+file_output = 'lcs.png'
+resolution = 200
+
+input_size = [] # string length
+dp = [] # time (S)
+brute = [] # time (S)
+
+f = open('output.txt', 'r')
+all_data = f.read()
+data = all_data.split()
+f.close()
+
+for i in range(len(data)):
+  if i%3 == 0:
+    input_size.append(int(data[i]))
+  elif i%3 == 1:
+    brute.append(float(data[i]))
+  else:
+    dp.append(float(data[i]))
+
+print(input_size)
+print(dp)
+print(brute)
+
+# figure,anvas,axes object
+fig = matplotlib.figure.Figure()
+canvas = matplotlib.backends.backend_agg.FigureCanvasAgg(fig)
+ax = fig.add_subplot(111)
+
+# plot(x, y)
+ax.plot(input_size, dp, label='dp')
+ax.plot(input_size, brute, label='brute')
+ax.set_xlim(500,6000)
+ax.set_ylim(0.0, 0.04)
+ax.set_xlabel('$input$ $size$ $n$')
+ax.set_ylabel('$time$ $(s)$')
+ax.set_title("dp vs. brute")
+ax.grid()
+ax.legend()
+fig.savefig(file_output, dpi = g_resolution)
